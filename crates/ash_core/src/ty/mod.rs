5e1003060ddf6ff2427bda888ff8940622cbf6df
noituri <yknomeh@protonmail.com>
2022-12-12 21:43:49 +0100

    Updated deps https://github.com/ash-language/ash/issues/21

--- crates/ash_core/src/ty/mod.rs
+++ crates/ash_core/src/ty/mod.rs
@@ -3,9 +3,11 @@ pub use function::FunctionType;
 pub use ty::*;
 // pub use type_system::*;
 pub use value::Value;
+pub use typing2::*;
 
 pub mod ast;
 pub mod function;
 pub mod ty;
 // pub mod type_system;
 pub mod value;
+pub mod typing2;
